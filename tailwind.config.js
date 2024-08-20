8de1a6132739fdcf6ebaa96cfeb687f474e68a92
Elena Chanou <elenachanou87@gmail.com>
2024-08-20 21:10:14 +0300

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Παράδειγμα: αν τα αρχεία σου βρίσκονται στον φάκελο src
+    "./public/**/*.html",   // Αν έχεις HTML αρχεία στον φάκελο public
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
